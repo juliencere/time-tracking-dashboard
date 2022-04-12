622321f4f2325d7767c525411842e836b20811bd
Julien <julien.cere.imboden@konverto.eu>
2022-04-12 14:54:57 +0200

    feat: add main adapter with viewbinding and databinding

---
+++ app/src/main/java/com/android/timetrackingdashboard/ui/main/adapter/MainAdapter.kt
@@ -0,0 +1,32 @@
+package com.android.timetrackingdashboard.ui.main.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.timetrackingdashboard.buisness.data.model.TimeData
+import com.android.timetrackingdashboard.databinding.TimeFrameItemBinding
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    private var items: List<TimeData> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TimeFrameItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun updateItems(images: List<TimeData>) {
+        this.items = images
+        notifyItemRangeChanged(0, images.size)
+    }
+
+    inner class ViewHolder(private val binding: TimeFrameItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
